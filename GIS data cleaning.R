33e19fbd33efea8444709f17c335bb3d928691a6
Punam Amratia <punam.amratia@Punams-MacBook-Pro.local>
2016-12-07 23:23:15 -0500

    Cleaned GIS script

--- GIS data cleaning.R
+++ GIS data cleaning.R
@@ -1,10 +1,25 @@
+######################################################################################
+############################    GIS CLEANING MIC 2011   ##############################
+######################################################################################
+#Notes: MICs 2011 has only been processed at this point owing to inability to automate 
+#loading of MODIS datasets. 2014 tiles are being loaded and processed currently and shall be
+#uploaded when available. For more information regarding automated scripts for MODIS downloads
+#please see MODISDownload.zip file in the github
+
 #Clean environment
 rm(list=ls(all=TRUE))
 
+#install appropriate packages
+list.of.packages <- c("dplyr", "rgdal", "raster", "RCurl", "tools", "gtools", "sp")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 #Import libraries
+library(dplyr)
 library(rgdal)
 library(raster)
 library(RCurl)
+library(tools)
 library(gtools)
 library(sp)
 #Bring in original raster data
@@ -22,6 +37,7 @@ raster_names <- file_path_sans_ext(c( unlist( lapply(strsplit(rasters,"[/]"), FU
 Ghana <- shapefile("Data/Rasters/GHA_adm/GHA_adm0.shp")
 
 #Use for loop to crop, resample extent and project raster and export to cleaned folder
+#NOTE: this loops takes a while, an already run version is stored in "cleaned" folders
 for(i in 1:length(rasters)){
   r=raster(rasters[i])
   r=crop(r, extent(Ghana))
@@ -45,16 +61,17 @@ MICS_gps <- read.csv("Data/GIS/MICS_mal.csv")
 MICS_gps$ID=as.numeric(row.names(MICS_gps)) 
 names(MICS_gps) 
 projection <- crs(all) 
-
 MICS_2011_loc <- SpatialPointsDataFrame(coords = MICS_gps[,4:5], data = MICS_gps, proj4string = projection) 
 crs(MICS_2011_loc) 
 plot(MICS_2011_loc, main="MICS locations") 
+
 #Extract Raster values for the points
 MICS_2011 <- extract(all, MICS_2011_loc, df=TRUE,method='simple')
+
 #these value for GS model need to be scaled
 MICS_2011=cbind(MICS_2011[1],scale(MICS_2011[,2:12]))
 
 #Merge back to .csv file and export
 MICS_gps %>% 
-left_join(MICS_2011, by="ID") %>% 
-write.csv("Data/Final_for analysis/MICS_2011.csv", row.names=F)
+  left_join(MICS_2011, by="ID") %>% 
+  write.csv("Data/Final_for analysis/MICS_2011.csv", row.names=F)
